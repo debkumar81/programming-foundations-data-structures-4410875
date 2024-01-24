10892cf4f27a2af40a805d8e48da776026dd2cb1
debkumar81 <debkumar04@gmail.com>
2024-01-24 12:37:16 +0000

    My changes

--- 02/02_09/02_09b/main.py
+++ 02/02_09/02_09b/main.py
@@ -1,4 +1,16 @@
 def find_second_smallest(my_list):
-    return 0
+    n = len(my_list)
+    if n==0 or n==1:
+        return None
+    smallest=float('inf')
+    second_smallest=float('inf')
+    for i in my_list:
+        if i>smallest and i<second_smallest:
+            second_smallest=i
+        elif i <smallest:
+            second_smallest=smallest
+            smallest=i
+        
+    return second_smallest
 
 print(find_second_smallest([5, 8, 3, 2, 6]))
